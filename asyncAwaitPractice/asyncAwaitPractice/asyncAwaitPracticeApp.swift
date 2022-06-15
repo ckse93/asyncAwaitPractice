e945591c00e99d58861d6a246bb98c152243cefa
ckse93@gmail.com <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>
2022-06-14 23:21:09 -0500

    adding project

---
+++ asyncAwaitPractice/asyncAwaitPractice/asyncAwaitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  asyncAwaitPracticeApp.swift
+//  asyncAwaitPractice
+//
+//  Created by Chan Jung on 6/14/22.
+//
+
+import SwiftUI
+
+@main
+struct asyncAwaitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
